81409c67b6ae75c9b5c12e8f022438ab1c8079bd
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-12-17 23:30:16 +0530

    category section added

---
+++ FlyAbroad-client/src/Components/Home/CategoryCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SecondaryButton from '../SecondaryButton';
+
+const CategoryCard = ({title, description, image}) => {
+  return (
+    <div className='rounded-xl hover:shadow-lg transition-all hover:bg-radial-[at_25%_25%] from-[#F5F5F5] to-white to-75% w-70 h-85 smL:w-70 xs:w-60 px-5 xs:px-3 py-8 text-center'>
+        <img src={image} className='mb-5 m-auto' alt="category image" />
+        <h2 className='text-2xl text-[#0B7077]'>{title}</h2>
+        <p className='py-3 mb-3'>{description}</p>
+        <div className='hover:text-[#0B7077]'>
+            <SecondaryButton name={"Explore Course"} />
+        </div>
+    </div>
+  )
+}
+
+export default CategoryCard;
--- FlyAbroad-client/src/Components/SecondaryButton.jsx
+++ FlyAbroad-client/src/Components/SecondaryButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SecondaryButton = ({name, isCurrent}) => {
   return (
     <div>
-    {isCurrent ? (<button className='border lg:text-[14px] md:text-[12px] text-[12px] w-fit px-4 py-1.5 rounded-md bg-[#09585e] text-white cursor-pointer'>{name}</button>) : (<button className='border lg:text-[14px] md:text-[12px] text-[12px] w-fit px-4 py-1.5 rounded-md text-[#818C96] hover:text-gray-600 cursor-pointer'>{name}</button>)}
+    {isCurrent ? (<button className='border lg:text-[14px] md:text-[12px] text-[12px] w-fit px-4 py-1.5 rounded-md bg-[#09585e] text-white cursor-pointer'>{name}</button>) : (<button className='border lg:text-[14px] md:text-[12px] text-[12px] w-fit px-4 py-1.5 rounded-md text-[#818C96] hover:text-[#FD661F] cursor-pointer'>{name}</button>)}
     </div>
   )
 }
