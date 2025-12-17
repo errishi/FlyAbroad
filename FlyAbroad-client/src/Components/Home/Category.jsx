3099e66d00122ad91063374f0d6b79a314de1b58
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-12-17 22:38:44 +0530

    mobile responsive

--- FlyAbroad-client/src/Components/Home/Category.jsx
+++ FlyAbroad-client/src/Components/Home/Category.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <div className='lg:px-15 md:px-10 px-7 py-5 mt-15'>
+      <div>
+        <h1 className='text-center font-bold lg:text-5xl md:text-4xl text-2xl text-[#0B7077]'>Courses Category</h1>
+        <img src="/category-Vector.svg" className='m-auto lg:pl-70 md:pl-50 pl-30 lg:pt-2 pt-1 md:w-80 lg:w-100 w-50' alt="vector" />
+      </div>
+    </div>
   )
 }
 
--- FlyAbroad-client/src/Components/Home/CourseCard.jsx
+++ FlyAbroad-client/src/Components/Home/CourseCard.jsx
@@ -3,18 +3,18 @@ import PrimaryButton from '../PrimaryButton';
 
 const CourseCard = ({image, studentsEnroll, title, description, price, discount, mrp }) => {
     return (
-        <div className='w-90 rounded-2xl overflow-clip relative bg-white'>
+        <div className='lg:w-90 w-auto rounded-2xl overflow-clip relative bg-white'>
             <img src={image} className='w-full' alt="course image" />
-            <div className='flex items-center absolute top-42 left-10 gap-3 bg-gray-100 px-3 py-1.5 rounded-full'>
-                <img src="/Group-profile.svg" alt="enrolled profile" />
-                <p>+ {studentsEnroll} Students</p>
+            <div className='flex items-center absolute lg:top-40 xs:top-34 smL:top-45 lg:left-8 xs:left-8 smL:left-18 gap-3 bg-gray-100 px-3 py-1.5 rounded-full'>
+                <img src="/Group-profile.svg" className='lg:w-auto md:w-auto xs:w-19' alt="enrolled profile" />
+                <p className='text-sm lg:text-[16px]'>+ {studentsEnroll} Students</p>
             </div>
             <div className='p-5 mt-5'>
                 <p className='text-sm text-gray-600'>1-28 Dec 2025</p>
                 <h2 className='font-bold text-xl text-[#0B7077]'>{title}</h2>
                 <p className='text-sm py-2 text-gray-500'>{description}</p>
-                <div className='flex justify-between items-center mt-5'>
-                    <h3 className='font-bold text-xl text-[#FD661F]'>$ {price}  &nbsp; &nbsp;<span className='text-gray-400 font-normal line-through'>$ {mrp}</span></h3>
+                <div className='flex lg:flex-row md:flex-row smL:flex-row xs:flex-col justify-between lg:items-center md:items-center items-start gap-3 mt-5'>
+                    <h3 className='font-bold text-xl text-[#FD661F]'>$ {price}  &nbsp; &nbsp;<span className='text-gray-400 font-normal line-through text-sm'>$ {mrp}</span></h3>
                     <div>
                         <PrimaryButton name={"Enroll Now"} />
                     </div>
