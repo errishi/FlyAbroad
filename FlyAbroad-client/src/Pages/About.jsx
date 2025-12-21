5068630a3c2188f9621594999964c14f48487dcc
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-12-21 17:15:03 +0530

    about contents

--- FlyAbroad-client/src/Pages/About.jsx
+++ FlyAbroad-client/src/Pages/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import AboutHeader from '../Components/AboutUs/AboutHeader';
+import AboutPlatform from '@/Components/AboutUs/AboutPlatform';
+import Story from '@/Components/AboutUs/Story';
 
 const About = () => {
   return (
     <div>
       <AboutHeader />
+      <AboutPlatform />
+      <Story />
     </div>
   )
 }
