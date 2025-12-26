e6b41da16f4e44203a154000f426a0adbde46713
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-12-26 17:07:20 +0530

    Not found page

---
+++ FlyAbroad-client/src/Pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='lg:px-15 md:px-10 px-7 py-5 text-center lg:text-3xl md:text-2xl text-xl font-semibold'>NotFound</div>
+  )
+}
+
+export default NotFound;
