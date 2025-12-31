05da5e3ebb13a4f29960521c1b96ccffb58f15a2
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-12-31 23:58:55 +0530

    bug fix

--- FlyAbroad-client/src/Components/Home/UniversityData.jsx
+++ FlyAbroad-client/src/Components/Home/UniversityData.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const UniversityData = () => {
   return (
-    <div className='flex justify-between lg:flex-row md:flex-row flex-col lg:px-20 lg:py-10 lg:mx-15 md:mx-10 px-7 py-5 mt-15 rounded-xl bg-[#11828a] text-white'>
+    <div className='flex justify-between lg:flex-row md:flex-row flex-col lg:px-20 lg:py-10 lg:mx-15 md:mx-10 mx-5 px-7 py-5 mt-15 rounded-xl bg-[#11828a] text-white'>
         <div className='flex flex-col justify-center mt-5 lg:mt-0 md:mt-0'>
             <h2 className='text-3xl lg:text-5xl md:text-4xl font-semibold text-center lg:text-start md:text-start'>Explore University</h2>
             <Link to={"/university"} className='w-fit m-auto lg:m-0 md:-0'>
