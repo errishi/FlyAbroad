60fe1b61d57e7dfd9590d7c64d3d312318db458f
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-01-26 15:12:27 +0530

    fix

--- FlyAbroad-client/src/Components/Home/Countries.jsx
+++ FlyAbroad-client/src/Components/Home/Countries.jsx
@@ -32,8 +32,8 @@ const countryData = [
 const Countries = () => {
   return (
     <div className='lg:px-15 md:px-10 m-auto px-7 py-5 mt-20'>
-        <h1 className='font-bold p-1 w-fit flex lg:items-center md:items-center text-center lg:text-4xl text-2xl drop-shadow-lg rounded bg-linear-to-l from-[#0B7077]/50 via-transparent to-transparent'>
-            <span className='text-[#FD661F]'><NearMeIcon sx={{ fontSize: 40 }} /></span> &nbsp; Where FlyAbroad can take you? &nbsp;
+        <h1 className='font-bold p-1 w-fit flex lg:gap-3 md:gap-3 gap-0 lg:items-center md:items-center text-center lg:text-4xl text-2xl drop-shadow-lg rounded bg-linear-to-l from-[#0B7077]/50 via-transparent to-transparent'>
+            <span className='text-[#FD661F]'><NearMeIcon sx={{ fontSize: 40 }} /></span> Where FlyAbroad can take you? &nbsp;
         </h1>
         <p className='lg:text-lg text-[16px] text-gray-500 my-3 lg:ml-15 lg:text-start md:text-start text-center'>Explore the Ultimate Destinations across the Globe!</p>
         <div className='flex flex-wrap gap-5 justify-center mt-15'>
