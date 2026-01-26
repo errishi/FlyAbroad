d4fdc7f09b63936040d285ec82bd9db156cadd46
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-01-26 15:13:15 +0530

    fix

--- FlyAbroad-client/src/Pages/Home.jsx
+++ FlyAbroad-client/src/Pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from '../Components/Home/Header';
 import PopularCourse from '../Components/Home/PopularCourse';
 import Category from '../Components/Home/Category';
-import Benefit from '../Components/Home/Benefit';
 import Exclusive from '../Components/Home/Exclusive';
 import UniversityData from '@/Components/Home/UniversityData';
 import StepsToApply from '@/Components/Home/StepsToApply';
@@ -17,7 +16,6 @@ const Home = ({setCurrentAuth, setReadFeedback, sendData}) => {
       <Category />
       <UniversityData />
       <Countries />
-      {/* <Benefit /> */}
       <StepsToApply setCurrentAuth={setCurrentAuth} />
       <StorySection />
       <Exclusive sendData={sendData} setReadFeedback={setReadFeedback} />
