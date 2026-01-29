cb1a955f6f34ad2002a48cb4b343f93bdcdb562c
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-01-29 22:59:55 +0530

    home page arrangement

--- FlyAbroad-client/src/Pages/Home.jsx
+++ FlyAbroad-client/src/Pages/Home.jsx
@@ -7,6 +7,7 @@ import Exams from '@/Components/Home/Exams';
 import Destination from '@/Components/Home/Destination';
 import StorySection from '@/Components/Home/StorySection';
 import ChooseUneFly from '@/Components/Home/ChooseUneFly';
+import Logos from '@/Components/Home/Logos';
 
 const Home = ({setCurrentAuth, setReadFeedback, sendData}) => {
   return (
@@ -14,6 +15,7 @@ const Home = ({setCurrentAuth, setReadFeedback, sendData}) => {
       <Header />
       <Exams />
       <Destination />
+      {/* <Logos /> */}
       <ChooseUneFly />
       <UniversityData />
       <StepsToApply setCurrentAuth={setCurrentAuth} />
