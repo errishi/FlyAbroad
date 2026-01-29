5ef161108190eb4f3435fe04470402d4e6c40dbf
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-01-29 23:17:41 +0530

    add new section

--- FlyAbroad-client/src/Pages/Home.jsx
+++ FlyAbroad-client/src/Pages/Home.jsx
@@ -8,6 +8,8 @@ import Destination from '@/Components/Home/Destination';
 import StorySection from '@/Components/Home/StorySection';
 import ChooseUneFly from '@/Components/Home/ChooseUneFly';
 import Logos from '@/Components/Home/Logos';
+import VideoTestimonialPage from '@/Components/Home/VideoTestimonialPage';
+import CTA from '@/Components/Home/CTA';
 
 const Home = ({setCurrentAuth, setReadFeedback, sendData}) => {
   return (
@@ -18,9 +20,11 @@ const Home = ({setCurrentAuth, setReadFeedback, sendData}) => {
       {/* <Logos /> */}
       <ChooseUneFly />
       <UniversityData />
+      <VideoTestimonialPage />
       <StepsToApply setCurrentAuth={setCurrentAuth} />
       <StorySection />
       <Exclusive sendData={sendData} setReadFeedback={setReadFeedback} />
+      <CTA />
     </div>
   )
 }
